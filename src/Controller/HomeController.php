4f517b98364a2ad7407e1aa5079641c0245ba161
Man <manbanhduc@gmail.com>
2023-01-13 15:05:34 +0100

    Create crud

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,38 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Customers;
+use App\Form\CustomerType;
 use App\Repository\CustomersRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_home')]
-    public function index(): Response
+    public function dashboard(): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-        ]);
-    }
-
-    #[Route('/clients', name: 'app_clients')]
-    public function clients(CustomersRepository $customers): Response
-    {
-        $customers = $customers->findAll();
-
-        return $this->render('home/clients.html.twig', [
-            'customers' => $customers
-        ]);
-    }
-
-    #[Route('/clients/{id}', name: 'app_client_show')]
-    public function clientShow(CustomersRepository $customers, $id): Response
-    {
-        $customer = $customers->findOneBy(['id' => $id]);
-
-        return $this->render('home/client.show.html.twig', [
-            'customer' => $customer
         ]);
     }
 
