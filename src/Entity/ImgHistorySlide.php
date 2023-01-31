23d5416b9e6d510dc765403201cc438ffc652a35
Man <manbanhduc@gmail.com>
2023-01-31 20:01:45 +0100

    Fix customer and services link

--- src/Entity/ImgHistorySlide.php
+++ src/Entity/ImgHistorySlide.php
@@ -8,7 +8,6 @@ use Doctrine\ORM\Mapping as ORM;
 // use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: ImgHistorySlideRepository::class)]
-
 #[ORM\HasLifecycleCallbacks]
 class ImgHistorySlide
 {
